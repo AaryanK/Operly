d53f85b18e1c4791fb889dff3068e0984269c4b7
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-10 04:46:08 +0545

    Export shared model provider client

--- packages/model_runtime/__init__.py
+++ packages/model_runtime/__init__.py
@@ -1,5 +1,12 @@
 """Shared model-runtime primitives used across OPERLY layers."""
 
+from .ollama_client import OllamaClient, OllamaError
 from .semantic_router import SemanticDecision, SemanticRouter, SemanticRoutingError
 
-__all__ = ["SemanticDecision", "SemanticRouter", "SemanticRoutingError"]
+__all__ = [
+    "OllamaClient",
+    "OllamaError",
+    "SemanticDecision",
+    "SemanticRouter",
+    "SemanticRoutingError",
+]
