3dfd1c4e0c8d19f5230b0142e53b7ae40cdabf35
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-10 09:57:26 +0545

    Export rewritten capability coding agent

--- packages/coding_harness/__init__.py
+++ packages/coding_harness/__init__.py
@@ -2,12 +2,13 @@
 
 from .engine import build_harness_plan
 from .evaluation import calculate_loss, compare_task, aggregate_report
-from .opencode_agent import OpenCodeStyleCodingAgent
+from .opencode_agent import CapabilityCodingAgent, OpenCodeStyleCodingAgent
 
 __all__ = [
     "build_harness_plan",
     "calculate_loss",
     "compare_task",
     "aggregate_report",
+    "CapabilityCodingAgent",
     "OpenCodeStyleCodingAgent",
 ]
