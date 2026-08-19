f29ebbf6ddb0ff0d4087306a9904bbdcfa582a2e
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-19 23:49:44 +0545

    add channel runtime package

---
+++ packages/channels/__init__.py
@@ -0,0 +1,12 @@
+from packages.channels.envelope import ChannelAttachment, ChannelEnvelope, ChannelResponse
+from packages.channels.identity import IdentityLinkConflict, IdentityService
+from packages.channels.service import ChannelService
+
+__all__ = [
+    "ChannelAttachment",
+    "ChannelEnvelope",
+    "ChannelResponse",
+    "IdentityLinkConflict",
+    "IdentityService",
+    "ChannelService",
+]
