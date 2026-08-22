9cdf077aa4c9f0e205f07a1f17a8425361db8810
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-22 19:01:06 +0545

    Export provider plugin registry

--- packages/model_runtime/__init__.py
+++ packages/model_runtime/__init__.py
@@ -1,15 +1,27 @@
 """Shared model-runtime primitives used across OPERLY layers."""
 
 from .ollama_client import OllamaClient, OllamaError
+from .openrouter_client import OpenRouterClient
 from .portfolio import ModelRoute, configured_portfolio, model_route
+from .providers import (
+    ModelClient,
+    installed_model_providers,
+    model_client_for_route,
+    register_model_provider,
+)
 from .semantic_router import SemanticDecision, SemanticRouter, SemanticRoutingError
 
 __all__ = [
     "OllamaClient",
     "OllamaError",
+    "OpenRouterClient",
+    "ModelClient",
     "ModelRoute",
     "configured_portfolio",
     "model_route",
+    "installed_model_providers",
+    "model_client_for_route",
+    "register_model_provider",
     "SemanticDecision",
     "SemanticRouter",
     "SemanticRoutingError",
