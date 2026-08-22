94e7834336f074bed09e44c069f074df7c7e8e69
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-23 01:49:03 +0545

    Expose ServiceBinding gateway API

---
+++ packages/service_bindings/__init__.py
@@ -0,0 +1,10 @@
+from .contracts import BindingCandidate, BindingInvocation, ServiceBinding
+from .service import CapabilityGateway, ServiceBindingResolver
+
+__all__ = [
+    "BindingCandidate",
+    "BindingInvocation",
+    "ServiceBinding",
+    "CapabilityGateway",
+    "ServiceBindingResolver",
+]
