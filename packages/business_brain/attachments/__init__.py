3768d56f1455d73fe665b9b150a3d2d95b051110
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-23 19:31:51 +0545

    Export attachment ingestion plugin

--- packages/business_brain/attachments/__init__.py
+++ packages/business_brain/attachments/__init__.py
@@ -1,3 +1,12 @@
 from .models import AttachmentInput, ParsedAttachment, AttachmentBundle, GeneratedOutput
 from .multimodal_processor import MultimodalProcessor
-__all__=["AttachmentInput","ParsedAttachment","AttachmentBundle","GeneratedOutput","MultimodalProcessor"]
+from .plugin import AttachmentIngestionPlugin
+
+__all__ = [
+    "AttachmentInput",
+    "ParsedAttachment",
+    "AttachmentBundle",
+    "GeneratedOutput",
+    "MultimodalProcessor",
+    "AttachmentIngestionPlugin",
+]
