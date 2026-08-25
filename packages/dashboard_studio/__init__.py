a26024fd00eba5638c502b1368ff0eeda958c9d7
Aaryan Kafle <66109257+AaryanK@users.noreply.github.com>
2026-08-26 03:55:07 +0545

    Delete retired Dashboard Studio package

--- packages/dashboard_studio/__init__.py
+++
@@ -1,3 +0,0 @@
-from .registry import REGISTRY, get_component, screen_manifest
-from .service import DashboardStudioService
-__all__=["REGISTRY","get_component","screen_manifest","DashboardStudioService"]
